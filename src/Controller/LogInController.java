5f9a6559486c4e81dd892d3cb0d0321183541147
fpe4 <fpe4@kth.se>
2021-05-12 10:43:27 +0200

    HELLO Removed!

--- src/Controller/LogInController.java
+++ src/Controller/LogInController.java
@@ -92,8 +92,6 @@ public class LogInController implements Initializable {
             return;
         }
 
-        System.out.println("HELLO!!");
-
         loginMessageLabel.setText("Logged in correctly!");
         loginMessageLabel.setTextFill(Color.GREEN);
 
